1e7a7c35f2a0b3a31b37b34b97e79c1a6f6da0e1
Federico <federicoburgos.dev@gmail.com>
2024-03-18 20:38:43 -0300

    boards-cards-grid

--- src/app/components/boards-cards-grid/boards-cards-grid.component.ts
+++ src/app/components/boards-cards-grid/boards-cards-grid.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { BoardPreviewCardComponent } from "../board-preview-card/board-preview-card.component";
+import { AppService } from '../../services/app.service';
 
 @Component({
     selector: 'app-boards-cards-grid',
@@ -17,5 +18,15 @@ import { BoardPreviewCardComponent } from "../board-preview-card/board-preview-c
 export class BoardsCardsGridComponent {
 
   @Input({required: true}) workspace: any;
+  appService = inject(AppService)
+
+  showCreateBoardModal(workspaceId: number) {
+    this.appService.setCreateBoardWorkspace(workspaceId);
+    let modal = document.getElementById('create-board-modal');
+    if (modal) {
+      // @ts-ignore
+      modal.showModal();
+    }
+  }
 
 }
