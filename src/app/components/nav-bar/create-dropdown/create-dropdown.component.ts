fa589399f25f2a27e638debfe962a1661479ca21
Federico <federicoburgos.dev@gmail.com>
2024-03-18 20:47:57 -0300

    create-dropdown

--- src/app/components/nav-bar/create-dropdown/create-dropdown.component.ts
+++ src/app/components/nav-bar/create-dropdown/create-dropdown.component.ts
@@ -1,9 +1,10 @@
 import { CdkMenuTrigger, CdkMenu, CdkMenuItem } from '@angular/cdk/menu';
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TrelloIconComponent } from "../../../icons/trello-icon/trello-icon.component";
 import { UsersIconComponent } from "../../../icons/users-icon/users-icon.component";
+import { AppService } from '../../../services/app.service';
 
 @Component({
     selector: 'app-create-dropdown',
@@ -11,17 +12,29 @@ import { UsersIconComponent } from "../../../icons/users-icon/users-icon.compone
     templateUrl: './create-dropdown.component.html',
     styleUrl: './create-dropdown.component.css',
     imports: [
-      CommonModule,
-      RouterModule,
-      CdkMenuTrigger,
-      CdkMenu,
-      CdkMenuItem,
-      TrelloIconComponent,
-      UsersIconComponent
+        CommonModule,
+        RouterModule,
+        CdkMenuTrigger,
+        CdkMenu,
+        CdkMenuItem,
+        TrelloIconComponent,
+        UsersIconComponent,
     ]
 })
 export class CreateDropdownComponent {
 
   @Input({required: true}) boardPage!: boolean;
+  @Input({ required: true }) board!: boolean;
+
+  appService = inject(AppService);
+
+  showCreateBoardModal() {
+    this.appService.setCreateBoardWorkspace();
+    let modal = document.getElementById('create-board-modal');
+    if (modal) {
+      // @ts-ignore
+      modal.showModal();
+    }
+  }
 
 }
