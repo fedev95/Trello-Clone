cf979699d0472a8c162f97976798051357384b0c
Federico <federicoburgos.dev@gmail.com>
2024-03-18 20:52:51 -0300

    recent-dropdown

--- src/app/components/nav-bar/recent-dropdown/recent-dropdown.component.ts
+++ src/app/components/nav-bar/recent-dropdown/recent-dropdown.component.ts
@@ -1,9 +1,10 @@
 import { CdkMenuTrigger, CdkMenu, CdkMenuItem } from '@angular/cdk/menu';
-import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AppService } from '../../../services/app.service';
 import { ChevronIconComponent } from "../../../icons/chevron-icon/chevron-icon.component";
+import { WorkspaceDataPipe } from "../../../pipes/workspace-data.pipe";
+import { BoardDataPipe } from "../../../pipes/board-data.pipe";
 
 @Component({
     selector: 'app-recent-dropdown',
@@ -11,46 +12,27 @@ import { ChevronIconComponent } from "../../../icons/chevron-icon/chevron-icon.c
     templateUrl: './recent-dropdown.component.html',
     styleUrl: './recent-dropdown.component.css',
     imports: [
-        CommonModule,
         RouterModule,
         CdkMenuTrigger,
         CdkMenu,
         CdkMenuItem,
-        ChevronIconComponent
+        ChevronIconComponent,
+        WorkspaceDataPipe,
+        BoardDataPipe
     ]
 })
 export class RecentDropdownComponent implements OnInit {
 
   @Input({required: true}) boardPage!: boolean;
+  @Input({ required: true }) board!: boolean;
 
   appService = inject(AppService);
-
-  data = this.appService.dataObservable;
-
-  recents: any[] = [];
+  data: any;
 
   ngOnInit(): void {
-    this.data.subscribe({
-      next: (res) => {
-        res.recent.forEach((element: any) => {
-          let workspaceAux;
-          let boardAux;
-          workspaceAux = res.workspaces.find((workspace: any) => workspace.id == element.workspaceId);
-          boardAux = workspaceAux.boards.find((board: any) => board.id == element.boardId);
-          this.recents.push(boardAux)
-        });
-      }
+    this.appService.getData().subscribe({
+      next: (res) => this.data = res
     });
-  }
-
-  searchWorkspace(workspaceId: number) {
-    let aux;
-    this.data.subscribe({
-      next: (res) => {
-        aux = res.workspaces.find((workspace: any) => workspace.id == workspaceId);        
-      }
-    })
-    return aux!.title;
   }
 
 }
