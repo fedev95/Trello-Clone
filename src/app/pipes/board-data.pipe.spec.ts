25cd395e0752cb7893f94005b2daa5ea27131ebb
Federico <federicoburgos.dev@gmail.com>
2024-03-18 21:02:16 -0300

    board-data pipe

---
+++ src/app/pipes/board-data.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { BoardDataPipe } from './board-data.pipe';
+
+describe('BoardDataPipe', () => {
+  it('create an instance', () => {
+    const pipe = new BoardDataPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
