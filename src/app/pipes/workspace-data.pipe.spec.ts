6d05f8181a90284831d371a3018f3af26680f33f
Federico <federicoburgos.dev@gmail.com>
2024-03-18 21:02:57 -0300

    workspace-data pipe

---
+++ src/app/pipes/workspace-data.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { WorkspaceDataPipe } from './workspace-data.pipe';
+
+describe('WorkspaceDataPipe', () => {
+  it('create an instance', () => {
+    const pipe = new WorkspaceDataPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
