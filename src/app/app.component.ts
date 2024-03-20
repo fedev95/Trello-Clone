db8b940e9e178ff50922b801c95ccea2c373fcec
Federico <federicoburgos.dev@gmail.com>
2024-03-20 14:41:24 -0300

    create-workspace-modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
 import { ThemeService } from './services/theme.service';
 import { CreateBoardModalComponent } from "./components/create-board-modal/create-board-modal.component";
+import { CreateWorkspaceModalComponent } from "./components/create-workspace-modal/create-workspace-modal.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    imports: [CommonModule, RouterOutlet, NavBarComponent, CreateBoardModalComponent]
+    imports: [CommonModule, RouterOutlet, NavBarComponent, CreateBoardModalComponent, CreateWorkspaceModalComponent]
 })
 export class AppComponent implements OnInit {
 
