fcb7f4d4403dd7490b62f522b323302487da4c88
Federico <federicoburgos.dev@gmail.com>
2024-04-04 16:34:09 -0300

    task card

--- src/app/components/task-card/task-card.component.ts
+++ src/app/components/task-card/task-card.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { PencilIconComponent } from "../../icons/pencil-icon/pencil-icon.component";
 
 @Component({
-  selector: 'app-task-card',
-  standalone: true,
-  imports: [],
-  templateUrl: './task-card.component.html',
-  styleUrl: './task-card.component.css'
+    selector: 'app-task-card',
+    standalone: true,
+    templateUrl: './task-card.component.html',
+    styleUrl: './task-card.component.css',
+    imports: [PencilIconComponent]
 })
 export class TaskCardComponent {
 
