7dfa3bf4d2130045e76ec9af12bb3d8a16df6cc4
Federico <federicoburgos.dev@gmail.com>
2024-04-09 13:38:17 -0300

    list-options cdkMenu

--- src/app/components/list-options/list-options.component.ts
+++ src/app/components/list-options/list-options.component.ts
@@ -1,7 +1,7 @@
-import { Component, ElementRef, HostListener, Input, ViewChild } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MenuDotsIconComponent } from "../../icons/menu-dots/menu-dots-icon.component";
-import { OverlayModule } from '@angular/cdk/overlay';
 import { XmarkIconComponent } from "../../icons/xmark-icon/xmark-icon.component";
+import { CdkMenu, CdkMenuItem, CdkMenuTrigger } from '@angular/cdk/menu';
 
 @Component({
     selector: 'app-list-options',
@@ -10,8 +10,10 @@ import { XmarkIconComponent } from "../../icons/xmark-icon/xmark-icon.component"
     styleUrl: './list-options.component.css',
     imports: [
         MenuDotsIconComponent,
-        OverlayModule,
-        XmarkIconComponent
+        XmarkIconComponent,
+        CdkMenu,
+        CdkMenuItem,
+        CdkMenuTrigger
     ]
 })
 export class ListOptionsComponent {
@@ -19,16 +21,6 @@ export class ListOptionsComponent {
   @Input({ required: true }) board: any;
   @Input({ required: true }) list: any;
   @Input({ required: true }) listIndex: number = 0;
-  isOpen: boolean = false;
-
-  @ViewChild('listOptions') menux!: ElementRef;
-  @ViewChild('triger') trigger!: ElementRef;
-  @HostListener('document:mousedown', ['$event'])
-  onClick(event: Event) {
-    if (this.isOpen && !this.menux.nativeElement.contains(event.target) && !this.trigger.nativeElement.contains(event.target)) {
-      this.isOpen = false;
-    }
-  }
 
   deleteList() {
     this.board.lists.splice(this.listIndex, 1);
